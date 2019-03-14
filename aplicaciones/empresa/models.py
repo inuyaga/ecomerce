59b07afa936a4e77682cdb566e4f06ed7c0f9040
Mario Esteban Hernandez <hdez.marioe@gmail.com>
2019-03-13 18:41:03 -0600

    urls

--- aplicaciones/empresa/models.py
+++ aplicaciones/empresa/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
+Usuario = get_user_model()
 # Create your models here.
+class Sucursal(models.Model):
+    suc_
--- ecomer/urls.py
+++ ecomer/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('emp/', include('aplicaciones.empresa.urls'), name='ec_empresa'),
+    path('pedido/', include('aplicaciones.pedido.urls'), name='ec_pedido'),
 ]
