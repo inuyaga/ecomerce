75a491853d30677264259ab22400395af11ab7d3
GusTaco92 <gustavo_gow1992@hotmail.com>
2019-03-14 17:29:01 -0600

    Actualizacion modelo pedido

--- aplicaciones/pedido/models.py
+++ aplicaciones/pedido/models.py
@@ -3,42 +3,42 @@ from aplicaciones.empresa.models import Sucursal
 from aplicaciones.usuario.models import User
 
 class Producto(models.Model):
-    codigo = models.CharField('código del producto', max_length=11)
-    rutaimg=models.ImageField("imagen", upload_to="imgPrductos/", height_field=None, width_field=None, max_length=None)
-    descripcion=models.CharField('Descripción del producto', max_length=400)
-    precio=models.FloatField("Precio de producto")
+    prod_codigo = models.CharField('código del producto', max_length=11)
+    prod_rutaimg=models.ImageField("imagen", upload_to="imgPrductos/", height_field=None, width_field=None, max_length=None)
+    prod_descripcion=models.CharField('Descripción del producto', max_length=400)
+    prod_precio=models.FloatField("Precio de producto")
     TIPOPRODUCTO=((1,"Papeleria"),(2,"Limpieza"))
-    tipo=models.IntegerField(choices=TIPOPRODUCTO)
-    estado_producto=models.BooleanField("Estatus del producto",default=True)
+    prod_tipo=models.IntegerField(choices=TIPOPRODUCTO)
+    prod_estado_producto=models.BooleanField("Estatus del producto",default=True)
     def __str__(self):
-            return self.codigo
+            return self.prod_codigo
 
 class Pedido(models.Model):
-    id_ped=models.AutoField(primary_key=True)
-    id_Suc=models.ForeignKey(Sucursal, verbose_name="Sucursales", on_delete=models.CASCADE)
-    fechaCreacion=models.DateTimeField("Fecha de creación", auto_now_add=True)
-    fechaAutorizacion= models.DateTimeField("Fecha de autorización")
-    fechaCancelacion= models.DateTimeField("Fecha de cancelación")
+    ped_id_ped=models.AutoField(primary_key=True)
+    ped_id_Suc=models.ForeignKey(Sucursal, verbose_name="Sucursales", on_delete=models.CASCADE)
+    ped_fechaCreacion=models.DateTimeField("Fecha de creación", auto_now_add=True)
+    ped_fechaAutorizacion= models.DateTimeField("Fecha de autorización")
+    ped_fechaCancelacion= models.DateTimeField("Fecha de cancelación")
     ESTADOPEDIDO=((1,"Pendiente por autorizar"),(2,"Autorizado"),(3,"Facturado"),(4,"Rechazado"),(5,"Entregado"))
-    estatusPedido=models.IntegerField(choices=ESTADOPEDIDO)
-    id_UsuarioCreo=models.ForeignKey(User, verbose_name="Creador de pedido", on_delete=models.SET_NULL,blank=True, null=True)
-    id_UsuarioAutorizo=models.ForeignKey(User, verbose_name="Autorizador", on_delete=models.SET_NULL,blank=True, null=True)
-    id_UsuarioCancelo=models.ForeignKey(User, verbose_name="Cancelador", on_delete=models.SET_NULL,blank=True, null=True)
-    pdffac=models.FileField("PDF de factura", upload_to="PdfFAC/", max_length=100)
-    xmlfac=models.FileField("XML de factura", upload_to="xmlFAC/", max_length=100)
-    fechaSubidaFac=models.DateTimeField("Carga de factura", auto_now=False, auto_now_add=False)
+    ped_estatusPedido=models.IntegerField(choices=ESTADOPEDIDO)
+    ped_id_UsuarioCreo=models.ForeignKey(User, verbose_name="Creador de pedido", related_name='creador', on_delete=models.SET_NULL,blank=True, null=True)
+    ped_id_UsuarioAutorizo=models.ForeignKey(User, verbose_name="Autorizador",related_name='autorizador', on_delete=models.SET_NULL,blank=True, null=True)
+    ped_id_UsuarioCancelo=models.ForeignKey(User, verbose_name="Cancelador",related_name='cancelo', on_delete=models.SET_NULL,blank=True, null=True)
+    ped_pdffac=models.FileField("PDF de factura", upload_to="PdfFAC/", max_length=100)
+    ped_xmlfac=models.FileField("XML de factura", upload_to="xmlFAC/", max_length=100)
+    ped_fechaSubidaFac=models.DateTimeField("Carga de factura", auto_now=False, auto_now_add=False)
     def __str__(self):
-        return str(self.id_ped)
+        return str(self.ped_id_ped)
 
 class DetallePedido(models.Model):
-    id_detalle=models.AutoField(primary_key=True)
-    id_pedido=models.ForeignKey(Pedido, verbose_name="Folio Pedido", on_delete=models.CASCADE)
-    cantidad=models.IntegerField("Cantidad de producto")
-    codigo = models.CharField('código del producto', max_length=11)
-    descripcion=models.CharField('Descripción del producto', max_length=400)
-    precio=models.FloatField("Precio de producto")
+    dtl_id_detalle=models.AutoField(primary_key=True)
+    dtl_id_pedido=models.ForeignKey(Pedido, verbose_name="Folio Pedido", on_delete=models.CASCADE)
+    dtl_cantidad=models.IntegerField("Cantidad de producto")
+    dtl_codigo = models.CharField('código del producto', max_length=11)
+    dtl_descripcion=models.CharField('Descripción del producto', max_length=400)
+    dtl_precio=models.FloatField("Precio de producto")
     TIPOPRODUCTO=((1,"Papeleria"),(2,"Limpieza"))
-    tipo=models.IntegerField(choices=TIPOPRODUCTO)
+    dtl_tipo=models.IntegerField(choices=TIPOPRODUCTO)
     def __str__(self):
-        return str(self.id_detalle)
+        return str(self.dtl_id_detalle)
     
